093c7292ea4fa457e83c925af2d10667541a44e7
Sunder91 <a_sunder_91@hotmail.com>
2020-11-02 21:52:56 -0600

    updated theme change. Needs work

--- DarkLightMode.js
+++ DarkLightMode.js
@@ -1,9 +1,13 @@
 
 function DarkLightMode(){
-    const theme = document.querySelector("#theme"); /*this slects the stylesheet*/
+    const theme = document.querySelector("#theme"); //this slects the stylesheet
     if (theme.getAttribute("href") == "MainTemplate.css"){
+       // localStorage.setItem('href', 'MainTemplateDark.css');
         theme.href = "MainTemplateDark.css";
     }else{
+        //localStorage.removeItem('href', 'MainTemplateDark.css');
+        //localStorage.setItem('href', 'MainTemplate.css');
+
         theme.href = "MainTemplate.css"
     }
     }
