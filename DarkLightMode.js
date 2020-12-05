4d3723410bcc91dcd78cde535df9e6d3c5b3cbe0
Sunder91 <a_sunder_91@hotmail.com>
2020-12-05 17:42:55 -0600

    Made changes to DarkLightMode.js.  Needs Work

--- DarkLightMode.js
+++ DarkLightMode.js
@@ -1,5 +1,17 @@
-
-function DarkLightMode(){
+        function DarkLightMode(){
+            function DarkLightMode(){
+                const theme = document.querySelector("#theme"); //this slects the stylesheet
+                if (theme.getAttribute("href") == "MainTemplate.css"){
+                   // localStorage.setItem('href', 'MainTemplateDark.css');
+                    theme.href = "MainTemplateDark.css";
+                }else{
+                    //localStorage.removeItem('href', 'MainTemplateDark.css');
+                    //localStorage.setItem('href', 'MainTemplate.css');
+            
+                    theme.href = "MainTemplate.css"        }
+        }
+/* this works
+      function DarkLightMode(){
     const theme = document.querySelector("#theme"); //this slects the stylesheet
     if (theme.getAttribute("href") == "MainTemplate.css"){
        // localStorage.setItem('href', 'MainTemplateDark.css');
@@ -11,22 +23,4 @@ function DarkLightMode(){
         theme.href = "MainTemplate.css"
     }
     }
-    
-/*
-        var theme = document.getElementById('theme');
-        if (theme) {
-            theme.addEventListener('change', function(event){
-                resetTheme(); //update color theme
-            });
-
-            function themeSwitch() {
-                if (theme.checked) {
-                    document.body.setAttribute('href', 'MainTemplate.css');
-                    localStorage.setItem('href', 'MainTemplate.css');
-                }else{
-                    document.body.removeAttribute('href');
-                    localStorage.removeItem(themeSwitch);
-                }
-            };
-        }
 */
