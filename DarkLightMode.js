d49fc2ff2babdace9f3665384a548dfc97340b4e
Sunder91 <a_sunder_91@hotmail.com>
2020-12-07 15:45:06 -0600

    Made changes to DarkLightMode.js

--- DarkLightMode.js
+++ DarkLightMode.js
@@ -1,11 +1,12 @@
             function DarkLightMode(){
+                //var theme = document.getElementById('href'); this line is not working
                 const theme = document.querySelector("#theme"); //this slects the stylesheet
                 if (theme.getAttribute("href") == "MainTemplate.css"){
-                   // localStorage.setItem('href', 'MainTemplateDark.css');
+                    localStorage.setItem('theme', 'Dark');
                     theme.href = "MainTemplateDark.css";
                 }else{
-                    //localStorage.removeItem('href', 'MainTemplateDark.css');
-                    //localStorage.setItem('href', 'MainTemplate.css');
+                    localStorage.removeItem('theme', 'Dark');
+                    localStorage.setItem('theme', 'Light');
             
                     theme.href = "MainTemplate.css"        
                 }
