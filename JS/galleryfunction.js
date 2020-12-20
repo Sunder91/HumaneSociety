06d35c175bcd280e9eb3576c04ca00625a17bee0
Sunder91 <a_sunder_91@hotmail.com>
2020-12-20 01:38:16 -0600

    More changes to galleryfunction.js  need to clean

--- JS/galleryfunction.js
+++ JS/galleryfunction.js
@@ -7,8 +7,8 @@
     dogPics.document.writeln("<div class='header'><h4>Humane Society of Polk County Logo Here</br>Non-Profit/Non-Kill animal shelter</h4></div>");
     dogPics.document.writeln("<h3><center>Shadow</center></h3>");
     dogPics.document.writeln("<div class='sidebar1'>Age: 3-4 years </br>Price: $160</br>Good with children: Unknown</br>Good with other dogs: Yes</br>Good with Cats:  No</div>");
-    dogPics.document.writeln("<div class='gallery'><img src='Images/Shadow4.jpg' alt='Shadow' width='300' height='300'></div>");
-    dogPics.document.writeln("</br>About Me:  I am a good boy that loves treats.  I love to play fetch and go for walks.  If you want a dog that loves nothing more than to cuddle with you I am your dog!");
+    dogPics.document.writeln("<div class='midbody'><div class='gallery'><img src='Images/Shadow4.jpg' alt='Shadow' width='300' height='300'></div></div>");
+    dogPics.document.writeln("<div class='sidebar2'></br>About Me:  I am a good boy that loves treats.  I love to play fetch and go for walks.  If you want a dog that loves nothing more than to cuddle with you I am your dog!</div>");
   }
 
 /*    This is how it was set up before changes to css on 12-20-2020
