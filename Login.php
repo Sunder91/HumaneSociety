be464fd8e3596011c566dd107f3ad66a2af221b9
Sunder91 <a_sunder_91@hotmail.com>
2021-11-23 21:52:30 -0600

    some small fixes

--- Login.php
+++ Login.php
@@ -1,5 +1,6 @@
 <!--used to have LoginAction.php code here-->
 <doctype html>
+    <html lang="en">
 <header>
 <meta charset ="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
